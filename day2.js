53427b83c4d31195c888f0237c2c7202e0c941dd
Sam Hamer <samuel.hamer@outlook.com>
2023-12-07 10:29:09 -0600

    day 2 part 1

--- day2.js
+++ day2.js
@@ -7,12 +7,38 @@ const cubes = {
 };
 
 const main = async () => {
-  const data = await fs.promises.readFile("test.txt", "utf8");
+  const data = await fs.promises.readFile("input.txt", "utf8");
   console.log(data);
 
   var sum = 0;
   var games = data.split("\n");
-  for (var i = 0; i < games.length; i++) {}
+  for (var i = 0; i < games.length; i++) {
+    var isGreater = false;
+    var temp = games[i]
+      .substring(games[i].indexOf(":") + 2)
+      .split(";")
+      .map((x) => x.split(","));
+
+    temp.forEach((element) => {
+      var num = element.map((x) => x.trim());
+      console.log(num);
+      num.forEach((ele) => {
+        console.log(ele);
+        var value = parseInt(ele);
+        console.log(cubes[ele.substring(ele.indexOf(" ") + 1)]);
+        console.log(value);
+        if (value > cubes[ele.substring(ele.indexOf(" ") + 1)]) {
+          console.log(value);
+          isGreater = true;
+        }
+      });
+    });
+    if (!isGreater) {
+      sum += i + 1;
+    }
+  }
+
+  console.log(sum);
 };
 
 main();
