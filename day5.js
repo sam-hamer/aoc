e5e97a9c5c0da353e552d0e6bb3c19dcf6bf1c48
Sam Hamer <samuel.hamer@outlook.com>
2023-12-17 13:39:06 -0600

    day 5 update

--- day5.js
+++ day5.js
@@ -1,72 +1,115 @@
 const fs = require("fs");
 
-const seedToSoil = [
-  {
-    min: 98,
-    max: 100,
-    relation: 50 - 98,
-  },
-  {
-    min: 50,
-    max: 97,
-    relation: 52 - 50,
-  },
-];
+const contentData = fs
+  .readFileSync("day5.txt", "utf8")
+  .split("\n\n")
+  .filter((x) => x.length);
 
-const runMap = (seeds, currentMap) => {
-  var newSeed = seed;
-  var newValues = [];
-  for (var i = 0; i < seeds.length; i++) {
-    var didMatch = false;
-    var seed = seeds[i];
-    for (var j = 0; j < currentMap.length; j++) {
-      var map = currentMap[j];
-      if (seed >= map.min && seed <= map.max) {
-        newValues.push(seed + map.relation);
-        didMatch = true;
-        break;
-      }
-    }
-    if (!didMatch) {
-      newValues.push(seed);
-    }
-  }
-  return newValues;
-};
+const seedValues = contentData.shift().split(": ")[1].split(" ");
 
-const createMaps = (input) => {
-  var maps = [];
-  for (var i = 0; i < input.length; i++) {
-    var map = [];
-
-    var splitMap = input[i].split("\n");
-    for (var j = 1; j < splitMap.length; j++) {
-      var splitLine = splitMap[j].split(" ").map(Number);
-      var min = splitLine[1];
-      var max = splitLine[1] + splitLine[2];
-      var relation = splitLine[0] - splitLine[1];
-      map.push({ min, max, relation });
-    }
-    maps.push(map);
-  }
-  return maps;
-};
-
-const main = async () => {
-  const data = fs.readFileSync("day5.txt", "utf8");
-  var splitMaps = data.split("\n\n");
-  var seeds = splitMaps
-    .shift()
-    .split(":")[1]
-    .split(" ")
-    .filter(Boolean)
-    .map(Number);
-  var maps = createMaps(splitMaps);
-
-  maps.forEach((map) => {
-    seeds = runMap(seeds, map);
+const seeds = [];
+for (let i = 0; i < seedValues.length; i += 2) {
+  seeds.push({
+    start: +seedValues[i],
+    length: +seedValues[i + 1],
   });
-  console.log(Math.min(...seeds));
-};
+}
+//console.log(JSON.stringify(seeds, null, 2));
 
-main();
+let highestValuePossible = 0;
+
+function createRange(line) {
+  const items = line.split(" ");
+  const range = {
+    dest: +items[0],
+    src: +items[1],
+    range: +items[2],
+  };
+
+  highestValuePossible = Math.max(
+    highestValuePossible,
+    range.src + range.range,
+    range.dest + range.range
+  );
+
+  return range;
+}
+
+function createNegativeRanges(ranges) {
+  ranges.sort((a, b) => a.src - b.src);
+
+  let start = 0;
+  for (let i = 0; i < ranges.length; i++) {
+    const range = ranges[i];
+    if (range.src > start) {
+      ranges.splice(i, 0, {
+        src: start,
+        dest: start,
+        range: range.src - start,
+      });
+      i++;
+    }
+    start = range.src + range.range;
+  }
+  return ranges;
+}
+
+function parseMap(data) {
+  const contents = data.split("\n").filter((x) => x);
+  const [from, _, to] = contents.shift().split(" ")[0].split("-");
+
+  return {
+    from: from,
+    to: to,
+    map: contents.map(createRange),
+  };
+}
+
+function walk(value, range, name, map) {
+  if (map[name] === undefined) {
+    return [value, range];
+  }
+
+  const item = map[name];
+  const rangeItem = item.map.find(
+    (x) => x.src <= value && value < x.src + x.range
+  );
+  if (rangeItem) {
+    const diff = value - rangeItem.src;
+    const newValue = rangeItem.dest + diff;
+    return walk(
+      newValue,
+      Math.min(range, rangeItem.range - diff),
+      item.to,
+      map
+    );
+  }
+
+  return walk(value, 1, item.to, map);
+}
+
+const parsed = contentData.map((x) => parseMap(x));
+parsed.forEach((p) => {
+  p.map = createNegativeRanges(p.map);
+});
+const parsedMap = parsed.reduce((acc, x) => {
+  acc[x.from] = x;
+  return acc;
+}, {});
+console.log(JSON.stringify(parsedMap, null, 4));
+let lowest = Infinity;
+for (const seed of seeds) {
+  let remaining = seed.length;
+  let start = seed.start;
+  while (remaining > 0) {
+    const [startLocation, consumed] = walk(start, remaining, "seed", parsedMap);
+
+    remaining -= consumed;
+    start += consumed;
+    if (startLocation < lowest) {
+      lowest = startLocation;
+    }
+  }
+}
+
+console.log(lowest);
