af34af23766bac1f0fbf52fd8c76f22d6c328334
Sam Hamer <samuel.hamer@outlook.com>
2024-12-01 19:35:12 -0600

    day 1

--- day1.js
+++ day1.js
@@ -1,52 +1,91 @@
 const fs = require("fs");
 
-const digits = {
-  one: "1",
-  two: "2",
-  three: "3",
-  four: "4",
-  five: "5",
-  six: "6",
-  seven: "7",
-  eight: "8",
-  nine: "9",
+const createAndSortArrays = (data) => {
+  let arrays = createArrays(data);
+
+  arrays.arr1.sort();
+  arrays.arr2.sort();
+
+  return arrays;
 };
 
-var findFirstDigit = function (str) {
-  for (var i = 0; i < str.length; i++) {
-    if (!isNaN(parseInt(str[i]))) {
-      return str[i];
-    } else if (Object.keys(digits).find((s) => str.startsWith(s, i))) {
-      var result =
-        digits[Object.keys(digits).find((s) => str.startsWith(s, i))];
-      return result;
+const createArrays = (data) => {
+  let dataArr = data.split("\n");
+  let arrays = {
+    arr1: [],
+    arr2: [],
+  };
+  dataArr.forEach((line) => {
+    const [val1, val2] = line.split("   ");
+    arrays.arr1.push(val1);
+    arrays.arr2.push(val2);
+  });
+  return arrays;
+};
+
+const createDicts = (data) => {
+  let dataArr = data.split("\n");
+  let maps = {
+    map1: new Map(),
+    map2: new Map(),
+  };
+  dataArr.forEach((line) => {
+    const [val1, val2] = line.split("   ");
+    if (maps.map1.has(val1)) {
+      maps.map1.set(val1, maps.map1.get(val1) + 1);
+    } else {
+      maps.map1.set(val1, 1);
     }
-  }
-};
-
-var findLastDigit = function (str) {
-  for (var i = str.length - 1; i >= 0; i--) {
-    if (!isNaN(parseInt(str[i]))) {
-      return str[i];
-    } else if (Object.keys(digits).find((s) => str.startsWith(s, i))) {
-      var result =
-        digits[Object.keys(digits).find((s) => str.startsWith(s, i))];
-      return result;
+    if (maps.map2.has(val2)) {
+      maps.map2.set(val2, maps.map2.get(val2) + 1);
+    } else {
+      maps.map2.set(val2, 1);
     }
-  }
+  });
+  return maps;
 };
 
-const main = async () => {
-  const data = await fs.promises.readFile("input.txt", "utf8");
-  fileContents = data; // data will be a string
+const sumDifferences = (arrays) => {
+  let arr1 = arrays.arr1;
+  let arr2 = arrays.arr2;
+  let sum = 0;
 
-  var strings = fileContents.split("\n");
-  var sum = 0;
-  for (var i = 0; i < strings.length; i++) {
-    let val = parseInt(findFirstDigit(strings[i]) + findLastDigit(strings[i]));
-    sum += val;
+  for (let i = 0; i < arr1.length; i++) {
+    sum += Math.abs(arr1[i] - arr2[i]);
   }
+  return sum;
+};
+
+const sumSimilarities = (dicts) => {
+  let map1 = dicts.map1;
+  let map2 = dicts.map2;
+  let sum = 0;
+
+  map1.forEach((value1, key1) => {
+    if (map2.has(key1)) {
+      sum += key1 * value1 * map2.get(key1);
+    }
+  });
+
+  return sum;
+};
+
+const getDifferences = () => {
+  let data = fs.readFileSync("day1.txt", "utf8");
+  let arrays = createAndSortArrays(data);
+  let sum = sumDifferences(arrays);
   console.log(sum);
 };
 
-main();
+const getSimilarityScore = () => {
+  let data = fs.readFileSync("day1.txt", "utf8");
+  let dicts = createDicts(data);
+  let similarity = sumSimilarities(dicts);
+  console.log(similarity);
+};
+
+if (process.argv[2] === "getDifferences") {
+  getDifferences();
+} else if (process.argv[2] === "getSimilarityScore") {
+  getSimilarityScore();
+}
